f9b8ca7c94a9e493b05883e27d0d19c8e137d1c1
GabrieleAltrocchi <gabriele.altrocchi@gmail.com>
2026-03-08 16:53:56 +0100

    The-US-States-Name-Game

---
+++ main.py
@@ -0,0 +1,41 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("The US States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+print(len(all_states))
+
+while len(guessed_states) < len(all_states):
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
+    print(answer_state)
+
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("missing_states.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state, font=("Arial", 8, "normal"))
+
+"""Another way to keep the code going while clicking instead of exit on click"""
+# def get_mouse_click_coor(x, y):
+#     print(x , y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+# screen.exitonclick()
